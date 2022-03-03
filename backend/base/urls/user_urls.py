9cbdc3b32b12c4a3c07178d854f70a1f5899d772
Raj Upadhyay <rupadhyay523@gmail.com>
2022-03-03 12:15:03 +0530

    v1.0.1

--- backend/base/urls/user_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from base.views import user_views as views
-
-urlpatterns = [
-    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('profile/',views.getUserProfile,name='users-profile'),
-    path('register/',views.registerUser,name='register'),
-    path('',views.getUsers,name='users'),
-]
